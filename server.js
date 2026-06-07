f75eb5fac8345ba4aaa455aeabe88e472438907a
Victor Gabriel <vbarros@addvaloraglobal.com>
2026-06-07 22:22:13 +0000

    Fix: express.static(__dirname) para servir index.html no Vercel

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 
 let db;
 
